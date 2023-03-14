9cb63dc31c05f7852432d7050f000d2fc6b56197
sylviajebet <sylviajebett@gmail.com>
2023-03-14 15:37:55 +0300

    SMS demo added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # at-demos
+
+Here is a walkthrough of AT APIs and their use.
+
+One product at a time, you'll get it :)
+
+
+### Lessgo! 
+
+- SMS Demo [here](./at-sms-demo/)
+
+- USSD Demo [here](./at-ussd-demo/)
+
---
+++ at-sms-demo/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// Authentication
+const credentials = {
+  apiKey: process.env.API_KEY, // API Key here, sandbox or live
+  username: process.env.USERNAME // Username: 'sandbox' for the test environment, App username for live
+};
+
+// Require the AT package
+const AfricasTalking = require("africastalking")(credentials);
+
+const sms = AfricasTalking.SMS;
+// Send SMS route
+router.post("/", (req, res) => {
+  const { to, message } = req.body || res.status(400).json({error: "Both 'to' and 'message' are required"});
+  sms
+    .send({ 
+        to: "+254***", // Number of recipient - can be multiple
+        message: "My message - is here!", // Message to be sent 
+        enque: true })
+    .then(response => {
+      console.log(response);
+      res.json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error.toString());
+    });
+});
+
+module.exports = router;
